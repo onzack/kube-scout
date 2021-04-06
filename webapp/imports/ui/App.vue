785d160955cd7b9a8aec2517c40ad09822e832d6
Domenic Mayer <dmayer@onzack.com>
2021-04-06 23:54:51 +0200

    Added grid layout

--- webapp/imports/ui/App.vue
+++ webapp/imports/ui/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
-    <app-nav />
-  
-    <main>
-      <router-view />
-    </main>
-
+    <div class="wrapper">
+      <div class="topMenu">
+        <app-nav />
+      </div>
+      <div class="leftMenu">
+        <router-link :to="{ name: 'Todo' }" exact>Todo</router-link><br/>
+        <router-link :to="{ name: 'About' }">About</router-link><br/>
+        <router-link :to="{ name: 'Hobbies' }">Hobbies</router-link><br/>
+      </div>
+      <div class="mainContent">
+        <main>
+          <router-view />
+        </main>
+      </div>
+    </div>
   </div>
 </template>
  
 <script>
-import AppNav from './components/navigation/Navigation.vue'
+import AppNav from './components/navigation/TopMenu.vue'
 
 export default {
   components: {
@@ -18,3 +27,25 @@ export default {
   }
 }
 </script>
+
+<style>
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-gap: 5 px;
+  grid-auto-rows: minmax(100px, auto);
+}
+.topMenu {
+  grid-column: 1 / 9;
+  grid-row: 1 ;
+}
+.leftMenu {
+  grid-column: 1;
+  grid-row: 2 / 9;
+  background-image: linear-gradient(to bottom, #d0edf5, #e1e5f0 100%);
+}
+.mainContent {
+  grid-column: 2 / 9;
+  grid-row: 2 / 9;
+}
+</style>
